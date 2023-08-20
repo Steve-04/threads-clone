e2c99dabd4614ba972621e4236345ecd1073352f
Steve-04 <vssteve004@gmail.com>
2023-08-20 17:27:03 +0530

    first commit

---
+++ lib/mongoose.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+let isConnected = false; // Variable to track the connection status
+
+export const connectToDB = async () => {
+  mongoose.set("strictQuery", true);
+
+  if (!process.env.MONGODB_URL) return console.log("Missing MongoDB URL");
+  if (isConnected) return console.log("MongoDB connection already established");
+    
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+
+    isConnected = true; // Set the connection status to true
+
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
